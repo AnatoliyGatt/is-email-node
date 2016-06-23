d6e0da7a7d56b41b85c3323107e81381461a9520
Anatoliy Gatt <anatoliy.gatt@aol.com>
2016-06-23 15:34:01 +0200

    Update is-email.js tests

--- test/is-email.js
+++ test/is-email.js
@@ -5,6 +5,7 @@ describe('is-email', function () {
     describe('init', function () {
         it('should return true if String is an email address', function () {
             assert.equal(isEmail('anatoliy.gatt@aol.com'), true, 'String should be an email address');
+            assert.equal(isEmail('a@b.c'), true, 'String should be an email address');
         });
 
         it('should return false if String is not an email address', function () {
